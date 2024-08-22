94f3541b00181d014a9eab94f42b503e438b7b09
Mestri27 <saurabhmestri27@gmail.com>
2024-08-22 13:43:09 +0530

    feat: add

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,14 +6,12 @@ export default function Document() {
       <Head>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet"/>
       <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"/>
-      <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet"/>
-      <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet"/>
+      
       </Head>
      
       <body>
         <Main />
         <NextScript />
-      <script src="../main"></script>
       </body>
     </Html>
   );
